e28b1593fc97f285f45df7615db93aeba4e9eb13
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-06-12 15:59:11 +0900

    fix variables name

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ const Slack = require("./slack.prototype")
 const request = async () => {
     const SlackInstance = new Slack()
     const channel = 'testtest'
-    const user_name='정권진'
+    const user_name = '정권진'
 
-    const text = []
+    const context = []
     const abc = ['a', 'b', 'c', 'd', 'e']
-    await SlackInstance.sendText({channel,user_name,text:abc})
-    await SlackInstance.sendTextField({ channel,user_name, text: abc })
+    await SlackInstance.sendText({ channel, user_name, context: abc })
+    await SlackInstance.sendTextField({ channel, user_name, context: abc })
 
-    text.push(SlackInstance.toCodeBlock(abc))
-    text.push(SlackInstance.toCode('hello'))
-    text.push(SlackInstance.toQuotes(abc))
-    text.push(SlackInstance.toOrderedList(abc))
-    text.push(SlackInstance.toLink({ name: 'byebye', url: 'https://www.youtube.com' }))
-    text.push(await SlackInstance.toMention('정권진'))
+    context.push(SlackInstance.toCodeBlock(abc))
+    context.push(SlackInstance.toCode('hello'))
+    context.push(SlackInstance.toQuotes(abc))
+    context.push(SlackInstance.toOrderedList(abc))
+    context.push(SlackInstance.toLink({ context: 'byebye', url: 'https://www.youtube.com' }))
+    context.push(await SlackInstance.toMention('정권진'))
 
-    await SlackInstance.sendText({ channel,user_name, text })
+    await SlackInstance.sendText({ channel, user_name, context })
 
     const columns = 'a b c d e f g'.split(' ')
     const data = ['1 2 3 4 5 6 7', '7 6 5 4 3 2 1'].map(e => e.split(' '))
     const initial_comment = SlackInstance.toCode("hello world")
     const file_name = 'file name'
 
-    await SlackInstance.sendTsvFile({ channel,user_name, columns, initial_comment, file_name, data })
+    await SlackInstance.sendTsvFile({ channel, user_name, columns, initial_comment, file_name, data })
 
 }
 request()
--- slack.prototype.js
+++ slack.prototype.js
@@ -13,65 +13,65 @@ Slack.prototype.getChannelID = async function ({ channel }) {
     return list.find((e) => e.name === channel).id
 }
 
-Slack.prototype.getUserID=async function({user_name}){
+Slack.prototype.getUserID = async function ({ user_name }) {
     const users = await this.web.users.list()
     return users.members.find((e) => e.real_name === user_name).id
 }
 
-Slack.prototype.sendTextField = async function ({ channel,user_name, text }) {
-    const fields = text.map(e => {
+Slack.prototype.sendTextField = async function ({ channel, user_name, context }) {
+    const fields = context.map(e => {
         return {
             type: 'mrkdwn',
             text: e
         }
     })
-    if(channel){
-        const channel_id=await this.getChannelID({channel})
+    if (channel) {
+        const channel_id = await this.getChannelID({ channel })
         await this.web.chat.postMessage({
-            channel:channel_id,
+            channel: channel_id,
             blocks: [{
                 type: 'section',
                 fields
             }]
         })
     }
-    if(user_name){
-        const user_id=await this.getUserID({user_name})
+    if (user_name) {
+        const user_id = await this.getUserID({ user_name })
         await this.web.chat.postMessage({
-            channel:user_id,
-            blocks:[{
-                type:'section',
+            channel: user_id,
+            blocks: [{
+                type: 'section',
                 fields
             }]
         })
     }
 }
 
-Slack.prototype.sendText = async function ({ channel,user_name, text }) {
-    if (Array.isArray(text)) {
-        text = text.join('\n')
+Slack.prototype.sendText = async function ({ channel, user_name, context }) {
+    if (Array.isArray(context)) {
+        context = context.join('\n')
     }
-    if(channel){
-        const channel_id=await this.getChannelID({channel})
+    if (channel) {
+        const channel_id = await this.getChannelID({ channel })
         await this.web.chat.postMessage({
-            channel:channel_id,
-            text
+            channel: channel_id,
+            text:context
         })
     }
-    if(user_name){
-        const user_id=await this.getUserID({user_name})
+    if (user_name) {
+        const user_id = await this.getUserID({ user_name })
         await this.web.chat.postMessage({
-            channel:user_id,
-            text
+            channel: user_id,
+            text:context
         })
     }
-    
+
 }
 
-Slack.prototype.sendTsvFile = async function ({ channel,user_name, file_name, columns, data, initial_comment = '' }) {
+Slack.prototype.sendTsvFile = async function ({ channel, user_name, file_name, columns, data, initial_comment = '' }) {
     const file = [columns, ...data].map(e => e.join('\t')).join('\n')
-    if(channel){
-        const channel_id=await this.getChannelID({channel})
+    if (channel) {
+        const channel_id = await this.getChannelID({ channel })
         await this.web.files.upload({
             channels: channel_id,
             filename: file_name,
@@ -80,8 +80,8 @@ Slack.prototype.sendTsvFile = async function ({ channel,user_name, file_name, co
             filetype: 'tsv'
         })
     }
-    if(user_name){
-        const user_id=await this.getUserID({user_name})
+    if (user_name) {
+        const user_id = await this.getUserID({ user_name })
         await this.web.files.upload({
             channels: user_id,
             filename: file_name,
@@ -90,7 +90,7 @@ Slack.prototype.sendTsvFile = async function ({ channel,user_name, file_name, co
             filetype: 'tsv'
         })
     }
-    
+
 }
 
 Slack.prototype.toBold = function (string) {
@@ -125,12 +125,12 @@ Slack.prototype.toUnorderedList = function (list) {
     return list.reduce((a, c) => a + `-  ${c}\n`, '')
 }
 
-Slack.prototype.toLink = function ({ name, url }) {
-    return `<${url}|${name}>`
+Slack.prototype.toLink = function ({ context, url }) {
+    return `<${url}|${context}>`
 }
 
 Slack.prototype.toMention = async function (user_name) {
-    const user_id = await this.getUserID({user_name})
+    const user_id = await this.getUserID({ user_name })
     return `<@${user_id}>`
 }
 module.exports = Slack
